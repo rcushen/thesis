3ed95f071e74435e3af21cf2c444890d04a7155c
Ryan Cushen <cushenr@gmail.com>
2021-06-19 23:09:31 +1000

    Further refactoring and improvements to document structure

--- functions.jl
+++ functions.jl
@@ -1,4 +1,6 @@
-using LinearAlgebra, Distributions
+using LinearAlgebra, Distributions, Distances
+
+include("nnlsq_pen.jl")
 
 """
     creategrid(min, max, resolution)
@@ -9,6 +11,9 @@ Output is a (`resolution` ^ 2) x 2 matrix, where each row is a grid point.
 
 """
 function creategrid(min::Number, max::Number, resolution::Int64)
+    range = max - min;
+    min += (range / resolution) / 2;
+    max -= (range / resolution) / 2;
     xs = LinRange(min, max, resolution);
     ys = LinRange(min, max, resolution);
     grid = [[x, y] for x in xs for y in ys];
@@ -20,12 +25,12 @@ end;
 """
     f_0(x, range; dist)
 
-Returns a density evaluation for some initial density f0.
+Returns a density evaluation of `x` for some initial density f0.
 
-Output is a normal or uniform pdf evaluated at x.
+Output is a pdf of `dist` evaluated at `x`.
 
 """
-function f0(x, range::Number; dist)
+function f0(x, range::Number; dist="uniform")
     if dist == "normal"
         d = MvNormal([range/2, range/2], 1)
         return pdf(d, x)
@@ -37,31 +42,59 @@ function f0(x, range::Number; dist)
 end;
 
 """
-    S(X)
+    sampledist(sample_size, width; dist)
 
-Returns an evaluation of the standard map on the matrix of points X.
+Returns a sample from the nominated distribution `dist, of size `sample_size`.
+
+Output is a `sample_size` x 2 matrix.
+
+"""
+function sampledist(sample_size, range::Number; dist="uniform")
+    if dist == "normal"
+        d = MvNormal([range/2, range/2], 1);
+        sample = rand(d, sample_size)';
+    elseif dist == "uniform"
+        sample = range .* rand(sample_size, 2)
+    end
+    sample = convert(Array{Float64}, sample)
+    return sample
+end
+
+"""
+    S(X; map_type)
+
+Returns the evaluation of a dynamical map on the matrix of points `X`, according to `map_type`.
 
 Output is a transformed matrix S(X) of same size.
 
 """
-function S(X)
-    a = 6;
-    result = [X[:,1] + X[:,2] X[:,2] + a*sin.(X[:,1] + X[:,2])];
-    result = mod.(result, 2π)
+function S(X; map_type)
+    if map_type == "standard"
+        a = 6;
+        result = [X[:,1] + X[:,2]  X[:,2] + a*sin.(X[:,1] + X[:,2])];
+        result = mod.(result, 2π);
+    elseif map_type == "cat"
+        result = [(2 * X[:,1]) + X[:,2]  X[:,1] + X[:,2]];
+        result = mod.(result, 2π);
+    elseif map_type == "identity"
+        result = X;
+    else
+        throw(ArgumentError(map_type, "unsupported map"))
+    end
     return result
 end;
 
 """
-    S_forward(X, S, n_steps)
+    S_forward(X, S, n_steps; map_type)
 
-Returns the result of ```S``` applied ```n_steps`` times, iteratively.
+Returns the result of `S` applied to `X ``n_steps` times, iteratively.
 
 Output is a transformed matrix S^n(X) of same size.
 
 """
-function S_forward(X, S, n_steps::Int64)
+function S_forward(X, S, n_steps::Int64; map_type)
     for t in 1:n_steps
-        X = S(X);
+        X = S(X; map_type);
     end
     return X
 end;
@@ -74,7 +107,7 @@ Returns the evaluation of ```x``` at the kernel defined by ```z``` and ```ϵ```.
 Output is a real scalar.
 
 """
-φ(x, z, ϵ) =  exp( -1 * ( norm(x - z) / ϵ ) ^ 2 );
+φ(x, z, ϵ) =  exp( -1 * ( peuclidean(x, z, [2π, 2π]) / ϵ ) ^ 2 );
 
 """
     evaluate_phi(sample, basis_locs, φ, ϵ)
@@ -99,31 +132,25 @@ end
 """
     integrate_phiy(Y, basis_locs, resolution)
 
-Integrates kernels φ centered at the image points ```Y``` over a Voronoi tesselation of the original ```basis_locs```.
+Integrates kernels φ centered at the image points `Y` over a Voronoi tesselation of the original `basis_locs`.
 
-Output is an n×m matrix of integral values, where n is the sample size (number of rows in ```Y```) and m is the number of bases (number of rows in ```basis_locs```)
+Output is an n×m matrix of integral values, where n is the sample size (number of rows in `Y`) and m is the number of bases (number of rows in `basis_locs`)
 
 """
-function integrate_phiy(Y, basis_locs, range::Number, resolution::Integer, ϵ)
-    n_fine_gridpoints = resolution ^ 2;
-    fine_grid = creategrid(0, range, resolution);
+function integrate_phiy(Y, basis_locs, range::Number, integral_resolution::Integer, ϵ)
+    n_fine_gridpoints = integral_resolution ^ 2;
+    fine_grid = creategrid(0, range, integral_resolution);
 
     n_bases = size(basis_locs, 1);
     sample_size = size(Y, 1);
 
-    distance_matrix = zeros(n_fine_gridpoints, n_bases);
-
-    for g in 1:n_fine_gridpoints
-        for b in 1:n_bases
-           distance_matrix[g,b] = norm(fine_grid[g,:] - basis_locs[b,:]);
-        end
-    end
+    distance_matrix = pairwise(PeriodicEuclidean([range, range]), fine_grid, basis_locs, dims=1)
 
     tile_indexes = argmin(distance_matrix, dims=2);
     tile_indexes = [ind[2] for ind in tile_indexes];
     tile_indexes = tile_indexes[:];
 
-    integral_values = zeros(sample_size, n_bases);
+    integral_values = Array{Float64}(undef, sample_size, n_bases);
 
     for j in 1:sample_size
         y_j = Y[j,:];
@@ -136,7 +163,7 @@ function integrate_phiy(Y, basis_locs, range::Number, resolution::Integer, ϵ)
             for i in 1:n_relevant_points
                 total += φ(Vb[i,:], y_j, ϵ);
             end
-            result = ((2π)^2 / sample_size) * total;
+            result = ((range^2) / n_fine_gridpoints) * total;
             integral_values[j,b] = result;
         end
     end
@@ -146,7 +173,7 @@ end;
 """
     construct_L(w, Φ, Ξ)
 
-Constructs the estimated matrix L, using the weights ```w```, the sample evaluation matrix ```Φ```, and the integral values ```Ξ```.
+Constructs the estimated matrix L, using the weights `w`, the sample evaluation matrix `Φ`, and the integral values `Ξ`, with integral weights `c`.
 
 Returns an m×m matrix, where m is the number of bases.
 """
@@ -154,7 +181,7 @@ function construct_L(w, Φ, Ξ, c)
     n_bases = size(Φ, 1);
     sample_size = size(Φ, 2);
 
-    L = zeros(n_bases, n_bases);
+    L = Array{Float64}(undef, n_bases, n_bases);
     for b in 1:n_bases
         for k in 1:n_bases
             val = 0;
@@ -192,7 +219,6 @@ function estimate_L(state_space, sample_size, S, φ, ϵ, basis_grid_size, integr
     X = sample;
     Y = S(X);
 
-
     basis_locs = creategrid(state_space["min"], state_space["max"], basis_grid_size);
     n_bases = basis_grid_size ^ 2;
 
@@ -204,4 +230,43 @@ function estimate_L(state_space, sample_size, S, φ, ϵ, basis_grid_size, integr
 
     L = construct_L(w, Φ, Ξ, c)
     return L
+end;
+
+"""
+    ordered_eigendecomp(M)
+
+Computes a sorted eigendecomposition of M.
+
+Returns a tuple of (lambda, Lambda), where lambda is the vector of eigenvalues and Lambda is the matrix of eigenvectors.
+
+"""
+function ordered_eigendecomp(M)
+    λ, Λ = eigvals(M), eigvecs(M)
+    p = sortperm(abs.(λ), rev=true)
+    λ = λ[p];
+    Λ = Λ[:,p];
+
+    return λ, Λ
+end;
+
+"""
+    basis_combination(grid, basis_locs, φ, ϵ, α)
+
+Computes a weighted sum of all basis functions, according to `alpha`.
+
+Returns a vector of length n, where n is the number of gridpoints.
+
+"""
+function basis_combination(grid, basis_locs, φ, ϵ, α)
+    n_bases = size(basis_locs, 1);
+    n_gridpoints = size(grid, 1);
+
+    evaluation_matrix = Array{Float64}(undef, n_gridpoints, n_bases);
+    for b in 1:n_bases
+        for n in 1:n_gridpoints
+            evaluation_matrix[n, b] = α[b] * φ(grid[n, :], basis_locs[b,:], ϵ);
+        end
+    end
+    evaluation_surface = sum(evaluation_matrix, dims=2)[:];
+    return evaluation_surface
 end;
---
+++ nnlsq_pen.jl
@@ -0,0 +1,32 @@
+using JuMP, Gurobi, LinearAlgebra
+
+function nnlsq_pen(A, b, x̄, p)
+    # Minimises \|Ax-b\|^2 + p\|x-x̄\|^2, subject to x≥0, where x̄ is the average value expected in the vector x, and p≥0 is a small scalar penalty
+    # Call with x, residual, objvalue = nnlsq(A, b, x̄, p)
+
+    model = Model(Gurobi.Optimizer)
+    # how to set optimizer parameters, not necessary here.
+    #    set_optimizer_attribute(model, "Method", 0)
+    #    set_optimizer_attribute(model, "Presolve", 0)
+
+    n1 = size(A, 1)
+    n2 = size(A, 2) 
+    
+    @variable(model, x[i=1:n2] ≥ 0)
+    @variable(model, residual[i=1:n1])
+     
+    @constraint(model, residual .== A * x - b)
+
+    #has to be quadratic penalty below.  Because sum of x ≈ constant and a linear penalty has no effect.  This is why the first penalty and the second penalty yield almost identical results.
+    @objective(model, Min, sum(residual.^2).+p*sum((x.-x̄).^2))
+    #the above yields almost identical results to the below, but very slightly better in terms of total weight accuracy
+    #@objective(model, Min, sum(residual.^2).+penalty*sum(x.^2))
+    
+    # Solve problem 
+    JuMP.optimize!(model)
+    println("Objective value is: ", JuMP.objective_value(model))
+
+    return JuMP.value.(x), JuMP.value.(residual), JuMP.objective_value(model)
+
+end
+
