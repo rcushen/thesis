e3c829c719b52c7a5bfc19598d095f620984d36c
Ryan Cushen <cushenr@gmail.com>
2021-06-21 22:02:01 +1000

    Improvement of Simulation #1

--- nnlsq_pen.jl
+++ nnlsq_pen.jl
@@ -29,27 +29,3 @@ function nnlsq_pen(A, b, x̄, p)
     return JuMP.value.(x), JuMP.value.(residual), JuMP.objective_value(model)
 
 end
-
-function nnlsq_pen_weighted(A, b, x̄, p, k)
-    # Minimises \|Ax-b\|^2 + p\|x-x̄\|^2, subject to x≥0, where x̄ is the average value expected in the vector x, and p≥0 is a small scalar penalty, and k is the same
-    # Call with x, residual, objvalue = nnlsq(A, b, x̄, p, k)
-
-    model = Model(Gurobi.Optimizer)
-
-    n1 = size(A, 1)
-    n2 = size(A, 2)
-
-    @variable(model, x[i=1:n2] ≥ 0)
-    @variable(model, residual[i=1:n1])
-
-    @constraint(model, residual .== A * x - b)
-
-    @objective(model, Min, sum(residual.^2) .+ p*sum((x.-x̄).^2) .+ k*(sum(x) - 1)^2)
-
-    # Solve problem
-    JuMP.optimize!(model)
-    println("Objective value is: ", JuMP.objective_value(model))
-
-    return JuMP.value.(x), JuMP.value.(residual), JuMP.objective_value(model)
-
-end
